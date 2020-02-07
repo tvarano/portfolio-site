03a1ff5276f82f1ed09a286e8b8c01e8579b0187
tvarano <tvarano54@gmail.com>
2020-02-07 15:33:27 -0500

    redo design

--- site/static/js/contact.js
+++ site/static/js/contact.js
@@ -1,20 +1,24 @@
 
 function copyResume() {
-    $("#copy-url-holder").attr("value", "http://" + document.URL.split('/')[2] + '/resume')
-    var copyText = document.getElementById("copy-url-holder");
-    copyText.select();
-    document.execCommand("copy");
-    copyText.blur()
-    $("#res-copy")[0].innerHTML="<p class='active'>Copied!</p>"
+    copy("http://" + document.URL.split('/')[2] + '/resume');
+    $("#res-copy")[0].innerHTML="<p class='active doc-action'>Copied!</p>"
+}
+
+function copy(str) {
+    const el = document.createElement('textarea');
+    el.value = str;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
 }
 
 function copyCard() {
-    $("#copy-url-holder").attr("value", "http://" + document.URL.split('/')[2] + '/static/src/business-card.pdf')
-    var copyText = document.getElementById("copy-url-holder");
-    copyText.select();
-    document.execCommand("copy");
-    copyText.blur()
-    $("#card-copy")[0].innerHTML="<p class='active'>Copied!</p>"
+    copy("http://" + document.URL.split('/')[2] + '/static/src/business-card.pdf')
+    $("#card-copy")[0].innerHTML="<p class='active doc-action'>Copied!</p>"
 }
 
 
@@ -43,7 +47,7 @@ function closeDoc(target) {
     query.find("button").each(function() {
         this.classList.remove("active")
         query.find(".doc-button.right")[0].innerHTML = "<p>Open</p>"
-        query.find(".doc-button.left")[0].innerHTML="<p>Copy Link</p>"
+        query.find(".doc-button.left")[0].innerHTML="<p class='doc-action'>Copy Link</p>"
     })
     setTimeout(function() {
         query.find(".doc-separator")[0].classList.remove("active")
@@ -62,7 +66,7 @@ function closeDocs() {
 // check clicks
 $(function() {
     $("body").click(function(e) {
-        console.log(e.target)
+        // console.log(e.target)
 
 /* if its a doc, or if its parent is a doc {
     if (it is open) close it
@@ -70,15 +74,20 @@ $(function() {
     
 } else close all
 */
-
+        console.log($(e.target).parents("button").prevObject[0].classList.contains("doc-action"))
         if (e.target.classList.contains("doc") || $(e.target).parents(".doc").length) {
+    
+    
             var target = (e.target.classList.contains("doc") ? e.target : $(e.target).parents(".doc")[0])
             if (target.classList.contains("active"))
                 closeDoc(target)
             else 
                 openDoc(target)
         } else {
-            closeDocs()
+            if (!($(e.target).parents("button").prevObject[0].classList.contains(".doc-action"))) {
+                console.log("DONE.")
+                closeDocs()
+            }
         }
     });
 })
